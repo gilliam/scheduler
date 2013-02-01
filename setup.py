95c9c61c2abd945cb125ae1511dc9aaf16c237d4
Johan Rydberg <johan.rydberg@gmail.com>
2013-02-01 22:12:09 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name="gilliam-orchestrator",
+    version="0.0",
+    packages=['xsnaga'],
+    scripts=['bin/gilliam-orchestrator'],
+    author="Johan Rydberg",
+    author_email="johan.rydberg@gmail.com",
+    description="Orchestrator for Gilliam",
+    license="Apache 2.0",
+    keywords="app platform",
+    url="https://github.com/gilliam/",
+)
